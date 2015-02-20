dd8d127ff4a9d9eb5a1581a2d94020f02cb3e666
Brian McClain <brianmmcclain@gmail.com>
2015-02-19 20:31:00 -0800

    Fixed binary list concatination, put reads in a loop so we don't stop listening when the client disconnects

--- tcp-echo/tcpecho.erl
+++ tcp-echo/tcpecho.erl
@@ -10,15 +10,19 @@ serve() ->
 	serve(9999).
 serve(Port) ->
 	{ok, LSock} = gen_tcp:listen(Port, [binary, {packet, 0}, {active, false}]),
+	listen_loop(LSock).
+
+listen_loop(LSock) ->
 	{ok, Sock} = gen_tcp:accept(LSock),
 	{ok, Bin} = do_recv(Sock, []),
-	ok = gen_tcp:close(Sock),
-	binary_to_list(Bin).
+	io:fwrite("~s~n", [Bin]),
+	listen_loop(LSock).	
 
 do_recv(Sock, Bs) ->
 	case gen_tcp:recv(Sock, 0) of
 		{ok, B} ->
-			do_recv(Sock, lists:append(Bs, B));
+			do_recv(Sock, lists:append(Bs, binary_to_list(B)));
 		{error, closed} ->
+			gen_tcp:close(Sock),
 			{ok, Bs}
 	end.
