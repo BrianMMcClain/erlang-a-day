7a89cfebd10ed3d47a37ae6cb7ffd1edc1889436
Brian McClain <brianmmcclain@gmail.com>
2015-02-22 23:22:47 -0500

    Make the app into something useful, pulled http server code from yesterday's project

--- rebar-app/src/rebarapp_app.erl
+++ rebar-app/src/rebarapp_app.erl
@@ -11,7 +11,23 @@
 
 start(_StartType, _StartArgs) ->
     lager:info("Child process started"),
+    ok = serve(),
     rebarapp_sup:start_link().
 
 stop(_State) ->
     ok.
+
+%% ===================================================================
+%% Internal methods
+%% ===================================================================
+
+serve() ->
+  {ok, _} = inets:start(httpd, [
+    {port,8081}, % Port to listen on
+    {server_name,"Erlang HTTPD Server"}, % Server name
+    {server_root,"log"}, % Log directory 
+    {document_root,"public"}, % Document root (HTML, JS, CSS)
+    {bind_address,{127,0,0,1}}, % Interface to bind on. NOTE: Defining "localhost" doesn't work here
+    {directory_index, ["index.html"]} % Index file
+  ]),
+  ok.
