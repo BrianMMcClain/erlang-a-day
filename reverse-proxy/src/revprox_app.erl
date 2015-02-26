863c8848841a17645233d32491e8600c1d87e0e9
Brian McClain <brianmmcclain@gmail.com>
2015-02-26 10:38:13 -0500

    Basic reverse proxy working, hard-coded for Github API currently until a config file is written

--- reverse-proxy/src/revprox_app.erl
+++ reverse-proxy/src/revprox_app.erl
@@ -11,7 +11,25 @@
 
 start(_StartType, _StartArgs) ->
     revprox_sup:start_link(),
+
+    % Start required applications
+    ensure_started(crypto),
+    ensure_started(asn1),
+    ensure_started(public_key),
+    ensure_started(ssl),
+    ensure_started(idna),
+    ensure_started(hackney),
+
+    % Start Elli link
     {ok, Pid} = elli:start_link([{callback, revprox_callback}, {port, 3000}]).
 
 stop(_State) ->
     ok.
+
+ensure_started(App) ->
+  case application:start(App) of
+    ok ->
+      ok;
+    {error, {already_started, App}} ->
+      ok
+  end.
--- reverse-proxy/src/revprox_callback.erl
+++ reverse-proxy/src/revprox_callback.erl
@@ -14,9 +14,30 @@ handle(Req, _Args) ->
 %     {ok, [], <<"Hello World!">>};
 
 handle(Method, Path, _Req) ->
-    {ok, [], <<"Request received">>}.
+    print_headers(_Req#req.headers),
+    NewURI = get_uri("https://api.github.com", Path),
+    {ok, StatusCode, RespHeaders, ClientRef} = hackney:request(Method, NewURI, [], list_to_binary([]), []),
+    {ok, Body} = hackney:body(ClientRef),
+    {StatusCode, RespHeaders, Body}.
 
 %% @doc: Handle request events, like request completed, exception
 %% thrown, client timeout, etc. Must return 'ok'.
 handle_event(_Event, _Data, _Args) ->
     ok.
+
+%% ==========================
+%% Private Methods
+%% ==========================
+
+get_uri(Host, []) ->
+  Host;
+get_uri(Host, Path) ->
+  StrPath = lists:map(fun(X) -> binary_to_list(X) end, Path),
+  Host ++ "/" ++ string:join(StrPath, "/").
+
+print_headers([]) ->
+  done;
+print_headers([{Header, Value} | Rest]) ->
+  io:format("~s: ~s~n", [binary_to_list(Header), binary_to_list(Value)]),
+  print_headers(Rest).
+
